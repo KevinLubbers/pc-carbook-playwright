5a449a70f24331577cd7f56385ff913173f4b47a
Kevin Lubbers <kelubbers@gmail.com>
2026-03-05 17:41:08 -0500

    Corrected division_list and properly loop through divisions in dropdown - properly loop through models in dropdown - properly extracting data from correct table - need to format extracted data better

--- main.py
+++ main.py
@@ -27,14 +27,28 @@ def run():
         page.click(".framework-header-selector-button")
 
         page.wait_for_selector(".component-selector-model-input")
-        division = page.eval_on_selector('select.component-selector-make-input','select => select.options[select.selectedIndex].textContent')
-        division_list = ['Toyota', 'Honda', 'Audi', 'BMW']
-        models = page.eval_on_selector_all(".component-selector-model-input option", "options => options.slice(1).map(option=> option.textContent)")
-        for model in models:
-            page.select_option('.component-selector-model-input', label=model)
-        
+        division_list = ['Toyota', 'Honda', 'Chevy Cars', 'Chevy Utility Vehicles']
+        for each_division in division_list:
+            page.select_option('.component-selector-make-input', label=each_division)
+            division = page.eval_on_selector('select.component-selector-make-input','select => select.options[select.selectedIndex].textContent')
             time.sleep(1)
-        print(f"Division: {division} \nModels: {models}")
+            page.wait_for_selector(".component-selector-model-input")
+            models = page.eval_on_selector_all(".component-selector-model-input option", "options => options.slice(1).map(option=> option.textContent)")
+            for model in models:
+                data = []
+                page.select_option('.component-selector-model-input', label=model)
+                time.sleep(1)
+                table = page.query_selector("table.style-cellTableWidget tbody")
+                rows = table.query_selector_all("tr")
+
+                for row in rows:
+                    cells = row.query_selector_all("td")
+                    row_data = [cell.inner_text().strip() for cell in cells]
+                    data.append(row_data)
+            
+                time.sleep(1)
+                print(data)
+            print(f"Division: {division} \nModels: {models}")
         page.pause()
 
         #browser.close()
